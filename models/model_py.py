4a01e21f26b466926a250401c005fb28331798c7
liwanlei <leileili126@163.com>
2017-09-12 17:40:02 +0800

    写model模块

--- models/model_py.py
+++ models/model_py.py
@@ -16,6 +16,11 @@ class User(Base):
 	leves=Column(Integer())
 	iphone=Column(Integer())
 	shebei=relationship('Shebei',backref='users')
+	file=relationship('FilePan',backref='users')
+	banben=relationship('BanbenWrite',backref='users')
+	testresult=relationship('TestResult',backref='users')
+	testcase=relationship('TestCase',backref='users')
+	buglog=relationship('BugLog',backref='users')
 	def __repr__(self):
 		return self.username
 	@classmethod
@@ -70,4 +75,166 @@ class Shebei(Base):
 	@classmethod
 	def get_count(cls):
 		return db_session.query(Shebei).count()
-
+class TestResult(Base):
+	__tablename__='testresults'
+	id=Column(Integer(),primary_key=True)
+	porject_id=Column(Integer(),ForeignKey('projects.id'))
+	creat_time=Column(DateTime())
+	bug_first=Column(Integer())
+	ceshirenyuan=Column(String(255))
+	is_send=Column(Boolean(),default=True)
+	user_id=Column(Integer(),ForeignKey('users.id'))
+	def __repr__(self):
+		return self.porject_name
+	@classmethod
+	def get_by_name(cls,name):
+		item=db_session.query(TestResult).filter(TestResult.porject_name==name).first()
+		return item
+	@classmethod
+	def get_by_id(cls,id):
+		item=db_session.query(TestResult).filter(TestResult.id==id).first()
+		return item
+	@classmethod
+	def get_by_user_id(cls,user_id):
+		item=db_session.query(TestResult).filter(TestResult.user_id==user_id).first()
+		return item
+class BanbenWrite(Base):
+	__tablename__='banbens'
+	id=Column(Integer(),primary_key=True)
+	porject_id=Column(Integer(),ForeignKey('projects.id'))
+	creat_time=Column(DateTime())
+	banbenhao=Column(String(32))
+	is_xian=Column(Boolean())
+	is_test=Column(Boolean())
+	test_user=Column(String(64))
+	user_id=Column(Integer(),ForeignKey('users.id'))
+	bugadmin=relationship('BugAdmin',backref='banbens')
+	def __repr__(self):
+		return self.porject_name
+	@classmethod
+	def get_by_name(cls,name):
+		item=db_session.query(BanbenWrite).filter(BanbenWrite.porject_name==name).first()
+		return item
+	@classmethod
+	def get_by_id(cls,id):
+		item=db_session.query(BanbenWrite).filter(BanbenWrite.id==id).first()
+		return item
+	@classmethod
+	def get_by_user_id(cls,user_id):
+		item=db_session.query(BanbenWrite).filter(BanbenWrite.user_id==user_id).first()
+		return item
+class FilePan(Base):
+	__tablename__='files'
+	id=Column(Integer(),primary_key=True)
+	file_fenlei=Column(String(64))
+	file_name=Column(String(64))
+	down_count=Column(Integer(),default=0)
+	creat_time=Column(DateTime())
+	down_url=Column(String(64))
+	is_tui=Column(Boolean(),default=False)
+	user_id=Column(Integer(),ForeignKey('users.id'))
+	def __repr__(self):
+		return self.file_name
+	@classmethod
+	def get_by_file_name(cls,name):
+		item=db_session.query(FilePan).filter(FilePan.file_name==name).first()
+		return item
+	@classmethod
+	def get_by_id(cls,id):
+		item=db_session.query(FilePan).filter(FilePan.id==id).first()
+		return item
+	@classmethod
+	def get_by_user_id(cls,user_id):
+		item=db_session.query(FilePan).filter(FilePan.user_id==user_id).first()
+		return item
+class BugAdmin(Base):
+	__tablename__='bugadmins'
+	id=Column(Integer(),primary_key=True)
+	porject_id=Column(Integer(),ForeignKey('projects.id'))
+	bugname=Column(String(64))
+	bugdengji=Column(String(64))
+	bug_miaoshu=Column(String(255))
+	ban_id=Column(Integer(),ForeignKey('banbens.id'))
+	fujian=Column(String(64))
+	is_que=Column(Boolean())
+	bug_status=Column(String(64))
+	bug_jiejuefangan=Column(String(64))
+	bug_send=Column(String(64))
+	bug_log=relationship('BugLog',backref='bugadmins')
+	user_id=Column(Integer(),ForeignKey('users.id'))
+	def __repr__(self):
+		return self.bugname
+	@classmethod
+	def get_by_bugname(cls,bugname):
+		item=db_session.query(BugAdmin).filter(BugAdmin.bugname==bugname).first()
+		return item
+	@classmethod
+	def get_by_id(cls,id):
+		item=db_session.query(BugAdmin).filter(BugAdmin.id==id).first()
+		return item
+	@classmethod
+	def get_by_porject_name(cls,porject_name):
+		item=db_session.query(BugAdmin).filter(BugAdmin.porject_name==porject_name).first()
+		return item
+class TestCase(Base):
+	__tablename__='testcases'
+	id=Column(Integer(),primary_key=True)
+	porject_name=Column(String(64))
+	casename=Column(String(64))
+	case_qianzhi=Column(String())
+	case_buzhou=Column(String())
+	case_yuqi=Column(String())
+	case_crea_time=Column(DateTime())
+	user_id=Column(Integer(),ForeignKey('users.id'))
+	def __repr__(self):
+		return self.casename
+	@classmethod
+	def get_by_project_name(Cls,project_name):
+		item=db_session.query(TestCase).filter(TestCase.project_name==project_name).first()
+		return item
+	@classmethod
+	def get_by_casename(Cls,casename):
+		item=db_session.query(TestCase).filter(TestCase.casename==casename).first()
+		return item
+	@classmethod
+	def get_by_id(Cls,id):
+		item=db_session.query(TestCase).filter(TestCase.id==id).first()
+		return item
+class BugLog(Base):
+	__tablename__='buglogs'
+	id=Column(Integer(),primary_key=True)
+	bug_id=Column(Integer(),ForeignKey('bugadmins.id'))
+	caozuo=Column(String())
+	caozuo_time=Column(DateTime())
+	user_id=Column(Integer(),ForeignKey('users.id'))
+	def __repr__(self):
+		return self.caozuo
+	@classmethod
+	def get_by_id(Cls,id):
+		item=db_session.query(BugLog).filter(BugLog.id==id).first()
+		return item
+	@classmethod
+	def get_by_user_id(Cls,user_id):
+		item=db_session.query(BugLog).filter(BugLog.user_id==user_id).first()
+		return item
+	@classmethod
+	def get_by_bug_id(Cls,bug_id):
+		item=db_session.query(BugLog).filter(BugLog.bug_id==bug_id).first()
+		return item
+class Project(Base):
+	__tablename__='projects'
+	id=Column(Integer(),primary_key=True)
+	name=Column(String(64))
+	bug_log=relationship('BugAdmin',backref='projects')
+	banben=relationship('BanbenWrite',backref='projects')
+	testresult=relationship('TestResult',backref='projects')
+	def __repr__(self):
+		return self.name
+	@classmethod
+	def get_by_id(cls,id):
+		item=db_session.query(Project).filter(Project.id==id).first()
+		return item 
+	@classmethod
+	def get_by_name(cls,name):
+		item=db_session.query(Project).filter(Project.name==name).first()
+		return item
